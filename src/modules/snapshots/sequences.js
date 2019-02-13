36a1cf442e175f82fe63e3169dba05970361a29e
Tomohiro Arakawa <tarakawa@purdue.edu>
2019-02-13 14:53:40 -0500

    show all devices regardless of the status

--- src/modules/snapshots/sequences.js
+++ src/modules/snapshots/sequences.js
@@ -1,30 +1,40 @@
-import { sequence } from 'cerebral';
-import { state, props } from 'cerebral/tags';
-import { set } from 'cerebral/operators';
-import _ from 'lodash';
+import { sequence } from "cerebral";
+import { state, props } from "cerebral/tags";
+import { set } from "cerebral/operators";
+import _ from "lodash";
 
 export const createSnapshots = sequence("createSnapshots", [
-    ({state, props}) => ({
-      unitList: _.without(Object.keys(state.get(`data`)), 'connection_id')
-    }),
-    ({state, props}) => ({
-      units: _.reduce(props.unitList, (acc, unit, unitkey) => {
-     
-        var unitData = state.get(`data.${[unit]}`); 
+  ({ state, props }) => ({
+    unitList: _.without(Object.keys(state.get(`data`)), "connection_id"),
+  }),
+  ({ state, props }) => ({
+    units: _.reduce(
+      props.unitList,
+      (acc, unit, unitkey) => {
+        console.log("unit " + unit);
+        var unitData = state.get(`data.${[unit]}`);
         //var health, location, connection, lastReport;
-        var lastDate =_.max(Object.keys(unitData || {}));
+        var lastDate = _.max(Object.keys(unitData || {}));
         var lastTime = _.max(Object.keys(unitData[lastDate] || {}));
-        var lastGPS = _.max(Object.keys(unitData[lastDate][lastTime].gps || {}));     
-        var lastHeartbeat = _.max(Object.keys(unitData[lastDate][lastTime].heartbeats || {}));     
-     
-				if (lastGPS && lastHeartbeat) {
+        var lastGPS = _.max(
+          Object.keys(unitData[lastDate][lastTime].gps || {}),
+        );
+        var lastHeartbeat = _.max(
+          Object.keys(unitData[lastDate][lastTime].heartbeats || {}),
+        );
 
-          var heartbeat = unitData[lastDate][lastTime].heartbeats[lastHeartbeat];     
-          var location = unitData[lastDate][lastTime].gps[lastGPS];     
+        if (lastGPS && lastHeartbeat) {
+          var heartbeat =
+            unitData[lastDate][lastTime].heartbeats[lastHeartbeat];
+          var location = unitData[lastDate][lastTime].gps[lastGPS];
           var lastReport = Math.round(
-            (Math.round((new Date()).getTime() / 1000 ) - _.max([lastHeartbeat, lastGPS])) / 60
-          ); 
-          var latency = heartbeat ? (heartbeat.recTime - heartbeat.genTime) : 'NA';
+            (Math.round(new Date().getTime() / 1000) -
+              _.max([lastHeartbeat, lastGPS])) /
+              60,
+          );
+          var latency = heartbeat
+            ? heartbeat.recTime - heartbeat.genTime
+            : "NA";
 
           var health = null;
           if (lastReport < 2) {
@@ -39,9 +49,9 @@ export const createSnapshots = sequence("createSnapshots", [
             health = "Down";
           }
 
-          var connection = null
+          var connection = null;
           if (health === "Healthy" || health === "Sick") {
-            connection = _.findKey(heartbeat.interfaces, 'active');
+            connection = _.findKey(heartbeat.interfaces, "active");
           } else {
             connection = "NA";
             lastReport = "NA";
@@ -53,11 +63,19 @@ export const createSnapshots = sequence("createSnapshots", [
             location,
             lastReport,
           };
+        } else {
+          acc[unit] = {
+            health: "Down",
+            connection: "NA",
+            location: "NA",
+            lastReport: "NA",
+          };
         }
+        console.log(acc);
         return acc;
-      }, {}),
-    }),
-    set(state`snapshots`, props`units`),
+      },
+      {},
+    ),
+  }),
+  set(state`snapshots`, props`units`),
 ]);
-
-
