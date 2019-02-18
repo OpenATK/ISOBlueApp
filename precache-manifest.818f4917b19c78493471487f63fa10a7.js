b249ac76a32bffa5f23580e207e46476de772d58
Tomohiro Arakawa <tarakawa@purdue.edu>
2019-02-18 11:26:25 -0500

    Updates

---
+++ precache-manifest.818f4917b19c78493471487f63fa10a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82a3724fda709b32a65e",
+    "url": "/ISOBlueApp/static/css/main.372dde05.chunk.css"
+  },
+  {
+    "revision": "82a3724fda709b32a65e",
+    "url": "/ISOBlueApp/static/js/main.82a3724f.chunk.js"
+  },
+  {
+    "revision": "5e65947a575a7f7b3f68",
+    "url": "/ISOBlueApp/static/js/runtime~main.5e65947a.js"
+  },
+  {
+    "revision": "f14f72d07af4650f2752",
+    "url": "/ISOBlueApp/static/js/2.f14f72d0.chunk.js"
+  },
+  {
+    "revision": "e8146c20a0dc02a14ec44e8723988692",
+    "url": "/ISOBlueApp/index.html"
+  }
+];
