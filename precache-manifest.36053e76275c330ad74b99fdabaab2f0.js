a870c07317ebab051af61679ba3a2e0cc3f347cb
Tomohiro Arakawa <tarakawa@purdue.edu>
2019-12-13 15:17:21 -0500

    Updates

---
+++ precache-manifest.36053e76275c330ad74b99fdabaab2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062b7e9390d4aa0b7193d7b71e1d3b4b",
+    "url": "/ISOBlueApp/index.html"
+  },
+  {
+    "revision": "169fd86daaa71e87b447",
+    "url": "/ISOBlueApp/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "169fd86daaa71e87b447",
+    "url": "/ISOBlueApp/static/js/2.72191e79.chunk.js"
+  },
+  {
+    "revision": "c805093a6cf6c38a6ebb8c7fc9ee33d5",
+    "url": "/ISOBlueApp/static/js/2.72191e79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "592356eca9d2e4251024",
+    "url": "/ISOBlueApp/static/js/main.005f2252.chunk.js"
+  },
+  {
+    "revision": "774e172e4eb929ef8f34",
+    "url": "/ISOBlueApp/static/js/runtime-main.ebdfa41d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ISOBlueApp/precache-manifest.2332caf4a3e45b86eab1ea5c5de98883.js"
+  "/ISOBlueApp/precache-manifest.36053e76275c330ad74b99fdabaab2f0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ISOBlueApp/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ISOBlueApp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
