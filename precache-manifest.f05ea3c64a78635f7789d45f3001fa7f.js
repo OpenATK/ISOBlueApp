f023ef607d76c1f33d222fcf319718fd65e7451b
Aaron Neustedter <aneusted@purdue.edu>
2020-08-07 12:39:51 -0400

    Updates

---
+++ precache-manifest.f05ea3c64a78635f7789d45f3001fa7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfca9a934b1c77662f48d15591ed6dc",
+    "url": "/ISOBlueApp/index.html"
+  },
+  {
+    "revision": "1c5e1d2271622f1ab44f",
+    "url": "/ISOBlueApp/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "1c5e1d2271622f1ab44f",
+    "url": "/ISOBlueApp/static/js/2.bfe431c3.chunk.js"
+  },
+  {
+    "revision": "cf6abf2e43f6f29f92d392795fb2fb3e",
+    "url": "/ISOBlueApp/static/js/2.bfe431c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ad8292cb6210db4efee",
+    "url": "/ISOBlueApp/static/js/main.8ba88593.chunk.js"
+  },
+  {
+    "revision": "774e172e4eb929ef8f34",
+    "url": "/ISOBlueApp/static/js/runtime-main.ebdfa41d.js"
+  }
+]);
